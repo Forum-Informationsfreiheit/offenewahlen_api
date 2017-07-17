8a276bd8e6c7b2ce3e9224e69671380b0af662ed
Stefan Kasberger <mail@stefankasberger.at>
2017-07-17 23:26:41 +0200

    fix minor issues after manual merge

--- offenewahlen_nrw17/urls.py
+++ offenewahlen_nrw17/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from . import views
 
 urlpatterns = [
     url(r'^', include('viz.urls')),

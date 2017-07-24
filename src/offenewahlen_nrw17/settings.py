40a74d6f761d3fec6d7258998c33a11c9953553b
Tobias Schönberg <tobias47n9e@gmail.com>
2017-07-24 21:31:11 +0200

    Add i18n template processor

--- src/offenewahlen_nrw17/settings.py
+++ src/offenewahlen_nrw17/settings.py
@@ -90,6 +90,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n'
             ],
         },
     },

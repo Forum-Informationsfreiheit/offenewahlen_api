711b36ff8beb3d5ed8708dc50d6fbc08fc882daa
Stefan Kasberger <mail@stefankasberger.at>
2017-07-28 22:54:14 +0200

    add parties.json; update data model; remove municipalities.json

--- src/viz/models.py
+++ src/viz/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 class Election(models.Model):
 	id = models.AutoField(primary_key=True)
-	name = models.CharField(max_length=200)
+	full_name = models.CharField(max_length=200)
 	short_name = models.CharField(max_length=100)
 	election_type = models.CharField(max_length=100)
-	spatial_dimension = models.CharField(max_length=100)
-	election_day = models.DateTimeField('timestamp of election day')
+	election_id = models.CharField(max_length=20) # from BMI
+	administrative_level = models.CharField(max_length=100) # municipality, district, state, federal
+	election_day = models.DateTimeField('timestamp of election day') # yyyy-mm-dd
 
 	def __str__(self):
 		return "%s" % (self.short_name)
@@ -16,12 +17,12 @@ class Election(models.Model):
 
 class Party(models.Model):
 	id = models.AutoField(primary_key=True)
-	party_id = models.CharField(max_length=20)
+	party_id = models.CharField(max_length=20) # from BMI
 	wikidata_id = models.CharField(max_length=20)
-	name = models.CharField(max_length=200)
-	short = models.CharField(max_length=20)
+	full_name = models.CharField(max_length=200)
+	short_name = models.CharField(max_length=20)
 	family = models.CharField(max_length=50)
-	description = models.CharField(max_length=200)
+	description = models.CharField(max_length=500)
 
 	def __str__(self):
 		return "%s" % (self.short)
@@ -31,32 +32,32 @@ class Party(models.Model):
 
 class Municipality(models.Model):
 	id = models.AutoField(primary_key=True)
-	spatial_id = models.CharField(max_length=20)
+	municipality_id = models.CharField(max_length=20) #Gemeindekennziffer
 	name = models.CharField(max_length=100)
 	district = models.CharField(max_length=100)
 	state = models.CharField(max_length=100)
 
 	def __str__(self):
-		return "%s %s" % (self.spatial_id, self.district)
+		return "%s %s" % (self.municipality_id, self.district)
 
 	class Meta:
-		ordering = ('spatial_id',)
+		ordering = ('municipality_id',)
 
 class MunicipalityResult(models.Model):
 	id = models.AutoField(primary_key=True)
-	spatial_id = models.ForeignKey(Municipality, on_delete=models.PROTECT)
+	municipality_id = models.ForeignKey(Municipality, on_delete=models.PROTECT)
 	eligible_voters = models.IntegerField(default=-1)
 	votes = models.IntegerField(default=-1)
 	valid = models.IntegerField(default=-1)
 	invalid = models.IntegerField(default=-1)
 	ts_result = models.DateTimeField('timestamp of bmi result')
-	is_final = models.BooleanField()
+	is_final = models.BooleanField() # is it final approved result for the municipality?
 
 	def __str__(self):
-		return "%s %s" % (self.spatial_id, self.ts_result)
+		return "%s %s" % (self.municipality_id, self.ts_result)
 
 	class Meta:
-		ordering = ('ts_result', 'spatial_id',)
+		ordering = ('ts_result', 'municipality_id',)
 
 class PartyResult(models.Model):
 	id = models.AutoField(primary_key=True)
@@ -72,11 +73,11 @@ class PartyResult(models.Model):
 
 class RawData(models.Model):
 	id = models.AutoField(primary_key=True)
-	timestamp = models.DateTimeField('creation date of BMI xml')
-	hash = models.CharField(max_length=100)
-	content = models.TextField()
-	header = models.TextField()
-	dataformat = models.CharField(max_length=10)
+	timestamp = models.DateTimeField('creation date of raw file')
+	hash = models.CharField(max_length=100) # hash of raw file
+	content = models.TextField() # raw data
+	header = models.TextField() # http header result
+	dataformat = models.CharField(max_length=10) # xml, csv, json, txt
 
 	def __str__(self):
 		return "%s" % (self.timestamp)

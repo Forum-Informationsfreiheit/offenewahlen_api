2e60f2c9523ea5204d1ad6d98bb87ad06ce24a91
Stefan Kasberger <mail@stefankasberger.at>
2017-08-09 01:36:03 +0200

    reroute urls and views

--- src/viz/urls.py
+++ src/viz/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^stats/', views.stats, name='stats'),
+	url(r'^$', views.index, name='viz'),
+	url(r'^viz/', views.viz, name='viz'),
+	url(r'^computing/', views.computing, name='computing'),
+	url(r'^waiting/', views.waiting, name='waiting'),
+	url(r'^test/', views.test, name='test'),
 ]
--- src/viz/views.py
+++ src/viz/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-
 def index(request):
-	return render(request, 'viz/index.dtl')
+	return render(request, 'viz/index_viz.dtl')
 
-def stats(request):
-	return render(request, 'viz/stats.dtl')
+def viz(request):
+	return render(request, 'viz/index_viz.dtl')
+
+def waiting(request):
+	return render(request, 'viz/index_waiting.dtl')
+
+def computing(request):
+	return render(request, 'viz/index_computing.dtl')
+
+def test(request):
+	return render(request, 'viz/index_test.dtl')

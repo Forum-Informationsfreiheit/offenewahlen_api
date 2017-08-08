0004103a5314bd177c0dc6dea58e9f4fc19e3113
Stefan Kasberger <mail@stefankasberger.at>
2017-08-09 01:38:17 +0200

    fix minor issue

--- src/viz/urls.py
+++ src/viz/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='viz'),
+	url(r'^$', views.viz, name='viz'),
 	url(r'^viz/', views.viz, name='viz'),
 	url(r'^computing/', views.computing, name='computing'),
 	url(r'^waiting/', views.waiting, name='waiting'),

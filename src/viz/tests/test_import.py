6d77b128844188385684ef98c0cb514e3b8e7b62
Tobias Schönberg <tobias47n9e@gmail.com>
2017-09-08 21:28:55 +0200

    Make import test run again

--- src/viz/tests/test_import.py
+++ src/viz/tests/test_import.py
@@ -1,10 +1,13 @@
+import datetime
 import os
 
 from unittest import TestCase
 from django.test import Client
 from django.core.management import call_command
+from django.utils import timezone
 
-from viz.models import RawData
+from viz.models import RawData, Election
+
 
 class ImportTest(TestCase):
 
@@ -12,14 +15,36 @@ class ImportTest(TestCase):
 	Simple test for views.
 	"""
 
+	def setUp(self):
+		"""
+		Set up the test class.
+		"""
+		election_time = datetime.datetime.strptime('2017-01-01', '%Y-%m-%d')
+		election_time = timezone.make_aware(election_time,
+				timezone.get_current_timezone())
+
+		Election.objects.update_or_create(
+			short_name = 'nrw17',
+			short_name_text = '',
+			full_name = '',
+			election_type = '',
+			wikidata_id = '',
+			administrative_level = '',
+			election_day = election_time
+		)
+
 	def test_run(self):
 		test_path = os.path.dirname(os.path.realpath(__file__))
 		local_data_file = test_path + '/data/test_data.xml'
-		call_command('importresults', local_path=local_data_file)
+		mapping_file = test_path + '/data/nrw_2017_mapping.json'
+
+		call_command('import_results', local_data_file, location='local',
+			file_type='xml', mapping_file=mapping_file)
 		number_of_results = RawData.objects.count()
 		self.assertEqual(number_of_results, 1)
 
-		call_command('importresults', local_path=local_data_file)
+		call_command('import_results', local_data_file, location='local',
+			file_type='xml', mapping_file=mapping_file)
 		number_of_results = RawData.objects.count()
 		self.assertEqual(number_of_results, 2)
 

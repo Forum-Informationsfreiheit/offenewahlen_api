56ccc953d023b60a1329c0db5529d56a4383407d
Tobias Schönberg <tobias47n9e@gmail.com>
2017-09-09 23:22:55 +0200

    Start moving API to rest framework

--- src/viz/urls.py
+++ src/viz/urls.py
@@ -5,8 +5,14 @@ from rest_framework.routers import DefaultRouter
 from . import views, views_api
 
 router = DefaultRouter()
-router.register(r'election', views_api.ElectionInterface,
-	base_name='election')
+router.register(r'election', views_api.ElectionInterface)
+router.register(r'district', views_api.DistrictInterface)
+router.register(r'municipality', views_api.MunicipalityInterface)
+router.register(r'party', views_api.PartyInterface)
+router.register(r'polling_station', views_api.PollingStationInterface)
+router.register(r'list', views_api.ListInterface)
+router.register(r'regional_electoral_district',
+	views_api.RegionalElectoralDistrictInterface)
 
 urlpatterns = [
 	url(r'^$', views.viz, name='viz'),
@@ -20,20 +26,6 @@ urlpatterns = [
 	url(r'^waiting/', views.waiting, name='waiting'),
 	url(r'^test/', views.test, name='test'),
 	url(r'^data/nrw13.csv$', views.serve_nrw13_csv, name='serve_nrw13_csv'),
-	url(r'^api/result/$', views.api_result, name='api_result'),
-	url(r'^api/result/nrw13/$', views.api_result_nrw13, name='api_result_nrw13'),
-	url(r'^api/result/nrw17/$', views.api_result_nrw17, name='api_result_nrw17'),
-	url(r'^api/base/$', views.api_base_party, name='api_base_party'),
-	url(r'^api/base/election/$', views.api_base_election, name='api_basedata_election'),
-	url(r'^api/base/municipality/$', views.api_base_municipality, name='api_base_municipality'),
-	url(r'^api/base/pollingstation/$', views.api_base_pollingstation, name='api_base_pollingstation'),
-	url(r'^api/base/list/$', views.api_base_list, name='api_basedata_list'),
-	url(r'^api/base/party/$', views.api_base_party, name='api_basedata_party'),
-	url(r'^api/base/list/$', views.api_base_list, name='api_basedata_list'),
-	url(r'^api/base/state/$', views.api_base_state, name='api_base_state'),
-	url(r'^api/base/district/$', views.api_base_district, name='api_base_district'),
-	url(r'^api/base/regionalelectoraldistrict/$', views.api_base_red, name='api_base_red'),
-	url(r'^api/raw/$', views.api_rawdata, name='api_rawdata'),
 	url(r'^api/geom/$', views.api_geom, name='api_geom'),
 	url(r'^api/', include(router.urls)),
 ]

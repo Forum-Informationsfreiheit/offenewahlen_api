71a622baeb3fe6e77923364f60fcbd27a9ea103f
Stefan Kasberger <mail@stefankasberger.at>
2018-05-05 12:40:31 +0200

    test testing

--- src/offenewahlen_api/settings_testing.py
+++ src/offenewahlen_api/settings_testing.py
@@ -6,18 +6,16 @@ TESTING = True
 SQLALCHEMY_ECHO = True
 
 if 'TRAVIS' in os.environ:
-    travis = os.getenv('TRAVIS', default=False)
-
-	DATABASES = {
+    DATABASES = {
 		'default': {
-			'ENGINE': 'django.db.backends.postgresql_psycopg2',
-			'NAME': 'travisci',
-			'USER': 'postgres',
-			'PASSWORD': '',
-			'HOST': 'localhost',
-			'PORT': '',
-	}
-}
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'travisci',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 elif os.environ['DATABASE_URL']:
     from offenewahlen_api.settings_user import *
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
